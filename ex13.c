3d5e1ddaf28dcab592c9507817a6f9bac2313a7f
garvieiraUni <gabrielarcanjo260107@gmail.com>
2025-05-28 11:42:49 -0300

    Delete ex13.c

--- ex13.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    float cf, final;
-    printf("Digite o preço de fábrica:");
-    scanf("%f",&cf);
-    final = cf*0.28+ cf*0.45;
-    printf("o preço final é de %f",cf+final);
-}
