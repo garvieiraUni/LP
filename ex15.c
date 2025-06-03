adae806e3586f3ea00538f1afb18cbab6880bf5f
garvieiraUni <gabrielarcanjo260107@gmail.com>
2025-06-03 09:05:24 -0300

    Delete ex15.c

--- ex15.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    int f;
-
-    printf("Digite a temperatura em farenheit:");
-    scanf("%i",&f);
-    printf("A temperatura em celsius é: %i", (5*(f-32))/9);
-}
